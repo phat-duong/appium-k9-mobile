b2dc26c7f70c43b609d57c8df262178fe697b280
Duong Tan Phat <phat.duong.henry@gmail.com>
2022-07-17 09:33:16 +0700

    lesson-15

---
+++ src/test/java/api_learning/LoginFormTest.java
@@ -0,0 +1,46 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import platform.Platform;
+
+public class LoginFormTest {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+        try {
+            //Navigate to Login screen
+            MobileElement navLoginScreenBtn = appiumDriver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginScreenBtn.click();
+
+            //Find Login Form Elements
+            MobileElement emailInputElem = appiumDriver.findElement(MobileBy.AccessibilityId("input-email"));
+            MobileElement passwordInputElem = appiumDriver.findElement(MobileBy.AccessibilityId("input-password"));
+            MobileElement loginButtonElem = appiumDriver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+
+            //Fill the form and login
+            emailInputElem.sendKeys("teo@sth.com");
+            passwordInputElem.sendKeys("12345678");
+            loginButtonElem.click();
+
+            //Verification | Login dialog
+            WebDriverWait wait = new WebDriverWait(appiumDriver, 5);
+            wait.until(ExpectedConditions.invisibilityOfElementLocated(MobileBy.id("android:id/alertTitle")));
+
+            MobileElement loginDialogTitleElem = appiumDriver.findElement(MobileBy.id("android:id/alertTitle"));
+            System.out.println(loginDialogTitleElem.getText());
+
+            //DEBUG PURPOSE ONLY
+            Thread.sleep(3000);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        appiumDriver.quit();
+    }
+}
